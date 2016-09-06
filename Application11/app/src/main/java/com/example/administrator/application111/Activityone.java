09b8217dc1d5bf0e613b11e34fd38a6ecaae18d9
yxj8791619 <18810911526@163.com>
2016-09-06 08:22:53 +0800

    anotherActivity3

---
+++ Application11/app/src/main/java/com/example/administrator/application111/Activityone.java
@@ -0,0 +1,45 @@
+package com.example.administrator.application111;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class Activityone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activityone);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Intent intent=getIntent();
+        String name=intent.getStringExtra("name");
+        Integer age=intent.getIntExtra("age", 20);
+        Toast.makeText(this,name+age,Toast.LENGTH_LONG).show();
+    }
+
+    public void back1(View view){
+        Intent intent=getIntent();
+        String name=intent.getStringExtra("name");
+        Integer age=intent.getIntExtra("age", 20);
+        intent.putExtra("result","姓名:"+name+" 年龄"+age);
+        setResult(0,intent);
+        finish();
+    }
+
+}
---
+++ Application11/app/src/main/java/com/example/administrator/application111/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.administrator.application111;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void turn1(View view){
+        Intent intent = new Intent(MainActivity.this,Activityone.class);
+        intent.putExtra("name","Zhangsan");
+        intent.putExtra("age",20);
+        startActivityForResult(intent,0);
+
+    }
+    public void turn2(View view){
+        Intent intent = new Intent(MainActivity.this,Activitytwo.class);
+        intent.putExtra("name","zhaosi");
+        intent.putExtra("age",20);
+        startActivityForResult(intent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==0 && resultCode==0){
+            String str=data.getStringExtra("result");
+            Toast.makeText(this,str, Toast.LENGTH_LONG).show();
+        }
+    }
+}
