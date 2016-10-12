e2ef464d0c025dd278f80e2819f8a46448fa5d8d
yxj8791619 <18810911526@163.com>
2016-10-12 14:56:59 +0800

    app Broadcast

---
+++ Application27/app/src/main/java/com/example/application_broadcast/MyReceiver.java
@@ -0,0 +1,17 @@
+package com.example.application_broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+    public MyReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String strMsg=intent.getStringExtra("broadcastmsg");
+        Toast.makeText(context,strMsg,Toast.LENGTH_LONG).show();
+    }
+}
