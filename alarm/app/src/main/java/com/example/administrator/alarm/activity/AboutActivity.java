a237adc25c81ce6ad099515053b711b0ef27f547
yxj8791619 <18810911526@163.com>
2016-12-02 13:39:44 +0800

    app alarm

---
+++ alarm/app/src/main/java/com/example/administrator/alarm/activity/AboutActivity.java
@@ -0,0 +1,18 @@
+package com.example.administrator.alarm.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.administrator.alarm.R;
+
+/**
+ *
+ * Created by zyascend on 2016/4/10.
+ */
+public class AboutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
